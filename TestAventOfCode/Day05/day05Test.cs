d5bbf04b2d9ba01c55ee861b58be8d02414765ee
Eric Leduc <eriled@gmail.com>
2024-01-08 07:40:45 -0500

    Day 05 part 01 test done

--- TestAventOfCode/Day05/day05Test.cs
+++ TestAventOfCode/Day05/day05Test.cs
@@ -7,7 +7,6 @@ namespace TestAventOfCode.Day05;
 public class day05Test
 {
     private string _input;
-    private string[]? _seeds;
 
     [SetUp]
     public void Setup()
@@ -22,15 +21,14 @@ public class day05Test
     public void ValidateTheExtractionOfTheSeedsList()
     {
         // Arrange
-        var expected = new[] {"79", "14", "55", "13"};
+        var expected = new List<int>() {79, 14, 55, 13};
         var interpreter = new Interpreter();
         
         // Act
         interpreter.GetSeeds(_input);
-        _seeds = interpreter.Seeds;
 
         // Assert compare the two arrays
-        Assert.That(_seeds, Is.EqualTo(expected));
+        Assert.That(interpreter.Seeds, Is.EqualTo(expected));
         
     }
     
@@ -45,7 +43,171 @@ public class day05Test
         interpreter.GetEachMap(model);
 
         // Assert compare the two arrays
-        Assert.That(interpreter.Dics[0].Count, Is.EqualTo(79));
-        
+        Assert.That(interpreter.Dics.Count, Is.EqualTo(7));
+    }  
+
+    [Test]
+    public void GetSmallestLocation()
+    {
+        // Arrange
+        var interpreter = new Interpreter();
+        interpreter.GetSeeds(_input);
+        var model = new Model(@".\Day05\input.txt");
+
+        // Act
+        interpreter.GetEachMap(model);
+        var smallestLocation = interpreter.GetSmallestLocation(interpreter.Seeds);
+
+        // Assert compare the two arrays
+        Assert.That(smallestLocation, Is.EqualTo(35));
+    } 
+    /*Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
+Seed 14, soil 14, fertilizer 53, water 49, light 42, temperature 42, humidity 43, location 43.
+Seed 55, soil 57, fertilizer 57, water 53, light 46, temperature 82, humidity 82, location 86.
+Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35, location 35.*/
+    [TestCase(79, 81)]
+    [TestCase(14, 14)]
+    [TestCase(55, 57)]
+    [TestCase(13, 13)]
+    [Test]
+    public void GetSoil(int seed, int soil)
+    {
+        // Arrange
+        var interpreter = new Interpreter();
+        interpreter.GetSeeds(_input);
+        var model = new Model(@".\Day05\input.txt");
+
+        // Act
+        interpreter.GetEachMap(model);
+        var smallestLocation = interpreter.GetSoil(seed);
+
+        // Assert compare the two arrays
+        Assert.That(smallestLocation, Is.EqualTo(soil));
     }
+    
+    [TestCase(81, 81)]
+    [TestCase(14, 53)]
+    [TestCase(57, 57)]
+    [TestCase(13, 52)]
+    [Test]
+    public void GetFertilizer(int soil, int expected)
+    {
+        // Arrange
+        var interpreter = new Interpreter();
+        interpreter.GetSeeds(_input);
+        var model = new Model(@".\Day05\input.txt");
+
+        // Act
+        interpreter.GetEachMap(model);
+        var fertilizer = interpreter.Getfertilizer(soil);
+
+        // Assert compare the two arrays
+        Assert.That(fertilizer, Is.EqualTo(expected));
+    }
+    
+    [TestCase(81, 81)]
+    [TestCase(53, 49)]
+    [TestCase(57, 53)]
+    [TestCase(52, 41)]
+    [Test]
+    public void GetWater(int fertilizer, int expected)
+    {
+        // Arrange
+        var interpreter = new Interpreter();
+        interpreter.GetSeeds(_input);
+        var model = new Model(@".\Day05\input.txt");
+
+        // Act
+        interpreter.GetEachMap(model);
+        var water = interpreter.GetWater(fertilizer);
+
+        // Assert compare the two arrays
+        Assert.That(water, Is.EqualTo(expected));
+    }
+    
+    [TestCase(81, 74)]
+    [TestCase(49, 42)]
+    [TestCase(53, 46)]
+    [TestCase(41, 34)]
+    [Test]
+    public void GetLight(int water, int expected)
+    {
+        // Arrange
+        var interpreter = new Interpreter();
+        interpreter.GetSeeds(_input);
+        var model = new Model(@".\Day05\input.txt");
+
+        // Act
+        interpreter.GetEachMap(model);
+        var light = interpreter.GetLight(water);
+
+        // Assert compare the two arrays
+        Assert.That(light, Is.EqualTo(expected));
+    }
+    
+
+    [TestCase(74, 78)]
+    [TestCase(42, 42)]
+    [TestCase(46, 82)]
+    [TestCase(34, 34)]
+    [Test]
+    public void GetTemperature(int light, int expected)
+    {
+        // Arrange
+        var interpreter = new Interpreter();
+        interpreter.GetSeeds(_input);
+        var model = new Model(@".\Day05\input.txt");
+
+        // Act
+        interpreter.GetEachMap(model);
+        var temperature = interpreter.GetTemperature(light);
+
+        // Assert compare the two arrays
+        Assert.That(temperature, Is.EqualTo(expected));
+    }
+    
+    [TestCase(78, 78)]
+    [TestCase(42, 43)]
+    [TestCase(82, 82)]
+    [TestCase(34, 35)]
+    [Test]
+    public void GetHumidity(int temperature, int expected)
+    {
+        // Arrange
+        var interpreter = new Interpreter();
+        interpreter.GetSeeds(_input);
+        var model = new Model(@".\Day05\input.txt");
+
+        // Act
+        interpreter.GetEachMap(model);
+        var humidity = interpreter.GetHumidity(temperature);
+
+        // Assert compare the two arrays
+        Assert.That(humidity, Is.EqualTo(expected));
+    }
+    
+    /*Seed 79, soil 81, fertilizer 81, water 81, light 74, temperature 78, humidity 78, location 82.
+Seed 14, soil 14, fertilizer 53, water 49, light 42, temperature 42, humidity 43, location 43.
+Seed 55, soil 57, fertilizer 57, water 53, light 46, temperature 82, humidity 82, location 86.
+Seed 13, soil 13, fertilizer 52, water 41, light 34, temperature 34, humidity 35, location 35.*/
+    [TestCase(78, 82)]
+    [TestCase(43, 43)]
+    [TestCase(82, 86)]
+    [TestCase(35, 35)]
+    [Test]
+    public void GetLocation(int humidity, int expected)
+    {
+        // Arrange
+        var interpreter = new Interpreter();
+        interpreter.GetSeeds(_input);
+        var model = new Model(@".\Day05\input.txt");
+
+        // Act
+        interpreter.GetEachMap(model);
+        var location = interpreter.GetLocation(humidity);
+
+        // Assert compare the two arrays
+        Assert.That(location, Is.EqualTo(expected));
+    }
+    
 }
--- day05/Interpreter.cs
+++ day05/Interpreter.cs
@@ -6,38 +6,30 @@ namespace day05;
 public class Interpreter
 {
     private Model _model;
-    public string[]? Seeds { get; private set; }
+    public List<int> Seeds { get; private set; }
 
-    public List<Dictionary<int,int>> Dics { get; set; } = new();
-    /*public Dictionary<int, int> Soil { get; set; } = new();
-    public Dictionary<int, int> Fertilizer { get; set; } = new();
-    public Dictionary<int, int> Water { get; set; } = new();
-    public Dictionary<int, int> Light { get; set; } = new();
-    public Dictionary<int, int> Temperature { get; set; } = new();
-    public Dictionary<int, int> Humidity { get; set; } = new();
-    public Dictionary<int, int> Location { get; set; } = new();*/
+    public List<Dictionary<int, int>> Dics { get; set; } = new();
 
     public void GetSeeds(string input)
     {
         // Regex: Matches "seeds: ", then any characters (except new line), optionally ending with a carriage return.
-        Seeds = Regex.Match(input, @"seeds: (.*)(\r)?").Groups[1].Value.Split(" ");
+        Seeds = Regex.Match(input, @"seeds: (.*)(\r)?").Groups[1].Value.Split(" ")
+            .Select(s => int.Parse(s)).ToList();
     }
 
     //dictionary<int,int> key = seed (Source) , value = destination
     private int GetMap(Dictionary<int, int> dic, int position)
     {
-        var input = _model.GameData[position];
-        //if input is empty return
-        if (input == "") return position;
-        var map = input.Split(" ");
         //loop until input is empty
-        while (_model.GameData[position++] != "" | _model.GameData.Length > position)
+        while (_model.GameData.Length > position)
         {
-            input = _model.GameData[position];
+            var input = _model.GameData[position];
             if (input == "") return position;
-            map = input.Split(" ");
+            var map = input.Split(" ");
             for (var i = 0; i < int.Parse(map[2]); i++) dic.Add(int.Parse(map[1]) + i, int.Parse(map[0]) + i);
+            position++;
         }
+
         return position;
     }
 
@@ -46,16 +38,80 @@ public class Interpreter
         _model = model;
         var position = 0;
         //create a list of string with the name of the dictionary
-        var list = new List<string> {"seed-to-soil map:", "soil-to-fertilizer map:", "fertilizer-to-water map:", "water-to-light map:", "light-to-temperature map:", "temperature-to-humidity map:", "humidity-to-location map:"};
-        foreach (var map in list)
+        var list = new List<string>
         {
+            "seed-to-soil map:", "soil-to-fertilizer map:", "fertilizer-to-water map:", "water-to-light map:",
+            "light-to-temperature map:", "temperature-to-humidity map:", "humidity-to-location map:"
+        };
+        foreach (var map in list)
             for (var i = position; i < model.GameData.Length; i++)
             {
-                if (!model.GameData[i].Contains(map)) continue;
-                var dic = new Dictionary<int, int>();
-                GetMap(dic, position + 1);
-                Dics.Add(dic);
+                position = i;
+                if (model.GameData[i].Contains(map))
+                {
+                    var dic = new Dictionary<int, int>();
+                    position = GetMap(dic, position + 1);
+                    Dics.Add(dic);
+                    break;
+                }
+            }
+    }
+
+    public int GetSmallestLocation(List<int> seeds)
+    {
+        var smallestLocation = 0;
+        foreach (var seed in seeds)
+        {
+            var location = GetLocation(GetHumidity(GetTemperature(GetLight(GetWater(Getfertilizer(GetSoil(seed)))))));
+            if (smallestLocation == 0 || location < smallestLocation)
+            {
+                smallestLocation = location;
             }
         }
+
+        return smallestLocation;
+    }
+
+    public int GetSoil(int seed)
+    {
+        var searchValueFind = 0;
+        Dics[0].TryGetValue(seed, out searchValueFind);
+        return searchValueFind == 0 ? seed : searchValueFind;
+    }
+    public int Getfertilizer(int soil)
+    {
+        var searchValueFind = 0;
+        Dics[1].TryGetValue(soil, out searchValueFind);
+        return searchValueFind == 0 ? soil : searchValueFind;
+    }
+    public int GetWater(int fertilizer)
+    {
+        var searchValueFind = 0;
+        Dics[2].TryGetValue(fertilizer, out searchValueFind);
+        return searchValueFind == 0 ? fertilizer : searchValueFind;
+    }
+    public int GetLight(int water)
+    {
+        var searchValueFind = 0;
+        Dics[3].TryGetValue(water, out searchValueFind);
+        return searchValueFind == 0 ? water : searchValueFind;
+    }
+    public int GetTemperature(int light)
+    {
+        var searchValueFind = 0;
+        Dics[4].TryGetValue(light, out searchValueFind);
+        return searchValueFind == 0 ? light : searchValueFind;
+    }
+    public int GetHumidity(int temperature)
+    {
+        var searchValueFind = 0;
+        Dics[5].TryGetValue(temperature, out searchValueFind);
+        return searchValueFind == 0 ? temperature : searchValueFind;
+    }
+    public int GetLocation(int humidity)
+    {
+        var searchValueFind = 0;
+        Dics[6].TryGetValue(humidity, out searchValueFind);
+        return searchValueFind == 0 ? humidity : searchValueFind;
     }
 }
